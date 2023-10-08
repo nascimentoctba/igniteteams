1f316c6d223eb548342568f55634d5785bdeef8c
Alexsandro <alexsandro.nv.02@gmail.com>
2023-10-08 17:50:42 -0300

    utilizando themas e definindo tipagens para o thema

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 
 export const Container =styled.View`
     flex: 1;
-    background-color:#000;
+    background-color:${({ theme }) => theme.COLORS.GRAY_600};
     align-Items: center;
     justify-Content: center;
 `;
